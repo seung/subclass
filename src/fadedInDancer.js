163c012a89ab88e4883fa23aeb77148618dd9dfa
Catalyst Students <accounts@catalystclass.com>
2012-12-05 11:09:57 -0800

    created color, fadein and picture dancers and passed test

--- src/fadedInDancer.js
+++ src/fadedInDancer.js
@@ -1,7 +1,8 @@
-// todo Comeback and fix the bug
-
 var makeFadedInDancer = function (left, top) {
 	var fadedInDancer = makeBlinkyDancer(left, top);
-	fadedInDancer.moneyMaker.fadeIn('slow');
+	fadedInDancer.moneyMaker.addClass("fadedindancer");
+	fadedInDancer.blink = function(){
+		fadedInDancer.moneyMaker.fadeIn(1500).delay(1500).fadeOut(1500);
+	};
 	return fadedInDancer;
 }
