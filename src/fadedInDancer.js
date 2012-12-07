0711f9e20dad851f32ba9bf862b217c99bee3cc1
Catalyst Students <accounts@catalystclass.com>
2012-12-07 10:30:34 -0800

    June's work on dance party

--- src/fadedInDancer.js
+++ src/fadedInDancer.js
@@ -1,6 +1,8 @@
 var makeFadedInDancer = function (left, top) {
 	var fadedInDancer = makeBlinkyDancer(left, top);
 	fadedInDancer.moneyMaker.addClass("fadedindancer");
+	var randomColorGenerator = ["#C12267","#D2B9D3","#78c7c7","#A0CFEC","#AFC7C7","#54C571","#FDD017","#F87217","#E799A3"]
+	fadedInDancer.moneyMaker.css({"border-color" : randomColorGenerator[Math.floor(Math.random()*9)]});
 	fadedInDancer.blink = function(){
 		fadedInDancer.moneyMaker.fadeIn(1500).delay(1500).fadeOut(1500);
 	};
