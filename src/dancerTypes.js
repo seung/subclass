2387cb5d394b102b9fdd53f49d938b52003feb48
Catalyst Students <accounts@catalystclass.com>
2012-12-08 15:30:49 -0800

    move more code into prototypes

--- src/dancerTypes.js
+++ src/dancerTypes.js
@@ -1,43 +1,45 @@
 var BlinkyDancer = function(left, top){
-      // we'll use top and left to set the position of this dancer
-    this.top = top;
-    this.left = left;
+    // we'll use top and left to set the position of this dancer
+  this.top = top;
+  this.left = left;
 
-    // used in setInterval below
-    this.frequency= Math.random() * 2000;
+  // used in setInterval below
+  this.frequency= Math.random() * 2000;
 
-    // get dressed...
-    this.moneyMaker = $("<span class='dancer'></span>");
+  // get dressed...
+  this.moneyMaker = $("<span class='dancer'></span>");
 
   this.getInPosition();
+}; // dancer
 
-  }; // dancer
-  
-BlinkyDancer.prototype.dance = function(){
-  // go out...
-  this.moneyMaker.appendTo(".stage");
-  // ...and do those sexy moves
-  var self = this;
-  setInterval(function(){
-    self.step();
-  }, this.frequency);
-};
 
-BlinkyDancer.prototype.step = function(){
-  this.getInPosition();
-  this.danceMove();
-};
+BlinkyDancer.prototype = {
+  dance: function(){
+    // go out...
+    this.moneyMaker.appendTo(".stage");
+    // ...and do those sexy moves
+    var self = this;
+    setInterval(function(){
+      self.step();
+    }, this.frequency);
+  },
 
-BlinkyDancer.prototype.getInPosition = function(){
-  var styleObj = {
-    top: this.top,
-    left: this.left
-  };
-  this.moneyMaker.css(styleObj);
-};
+  step: function(){
+    this.getInPosition();
+    this.danceMove();
+  },
 
-BlinkyDancer.prototype.danceMove = function(){
-  this.moneyMaker.toggle();
+  getInPosition: function(){
+    var styleObj = {
+      top: this.top,
+      left: this.left
+    };
+    this.moneyMaker.css(styleObj);
+  },
+
+  danceMove: function(){
+    this.moneyMaker.toggle();
+  }
 };
 
 
@@ -48,15 +50,17 @@ var RaveDancer = function(left, top) {
   this.left = left;
   this.frequency = 70 ;
   console.log("raveDancer");
-  this.danceMove = function(){
-    var randomColorGen = function() {
-      var raveColors = ['rgb(255, 0, 204)','rgb(247, 255, 0)','rgb(94, 255, 0)','rgb(0, 255, 242)'];
-      return raveColors[Math.floor(Math.random() * raveColors.length)];
-    };
-
-    var randomColor = randomColorGen(); 
-    this.moneyMaker.css("border-color", randomColor);
-  };
 };
 
+
 RaveDancer.prototype = new BlinkyDancer();
+
+RaveDancer.prototype.danceMove = function(){
+  var randomColorGen = function() {
+    var raveColors = ['rgb(255, 0, 204)','rgb(247, 255, 0)','rgb(94, 255, 0)','rgb(0, 255, 242)'];
+    return raveColors[Math.floor(Math.random() * raveColors.length)];
+  };
+  var randomColor = randomColorGen(); 
+  this.moneyMaker.css("border-color", randomColor);
+};
+
