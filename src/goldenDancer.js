c7dfa14e722602d337b26820edf8af983742a0db
Catalyst Students <accounts@catalystclass.com>
2012-12-09 11:35:40 -0800

    Refactored Dancer and GoldenDancer into prototype methods

--- src/goldenDancer.js
+++ src/goldenDancer.js
@@ -1,12 +1,16 @@
 var GoldenDancer = function(left, top) {
 	Dancer.apply(this, [left, top]);
-	var self = this;
-	this.blink = function() {
-      self.moneyMaker.fadeToggle()
-	};
 	this.frequency = 1000;
-	this.moneyMaker.addClass('moonwalk');
-	this.moneyMaker.css({border: "10px solid goldenrod"});	
+	this.cssSetup();
 };
 
 GoldenDancer.prototype = new Dancer();
+
+GoldenDancer.prototype.blink = function() {
+	this.moneyMaker.fadeToggle()
+};
+
+GoldenDancer.prototype.cssSetup = function() {
+	this.moneyMaker.addClass('moonwalk');
+	this.moneyMaker.css({border: "10px solid goldenrod"});	
+};
