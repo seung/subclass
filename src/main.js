4e5348f15c2675a68afe9195ce689bf47d69ba6d
Catalyst Students <accounts@catalystclass.com>
2013-01-26 15:12:05 -0800

    adds two new dance classes

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
    * there's just one, but as you create more types, you'll need
    * to add their factory functions here. */
   var kindsOfDancers = {
-    makeBlinkyDancer: makeBlinkyDancer // found in blinkyDancer.js
+    makeBlinkyDancer: makeBlinkyDancer, // found in blinkyDancer.js
+    makeTinyDancer: makeTinyDancer, // found in tinyDancer.js
+    makePrivateDancer: makePrivateDancer // found in privateDancer.js
   };
 
   /* This is a list of all the dancers that have been created.
---
+++ src/tinyDancer.js
@@ -0,0 +1,53 @@
+var makeTinyDancer = function(left, top){
+  /* Creates and returns a new dancer object at the given position,
+   * where left is x-coordinate of left side and top is y-coordinate
+   * of top side (measured down from top of window). */
+
+  var dancer = {
+    // we'll use top and left to set the position of this dancer
+    top: top,
+    left: left,
+
+    // used in setInterval below
+    frequency: Math.random() * 2000,
+
+    // get dressed... (use jQuery to create an HTML <span> tag)
+    $moneyMaker: $('<span class="tinyDancer"></span>'),
+
+    dance: function(){
+      // go out...  (add our tag to the HTML page)
+      dancer.$moneyMaker.appendTo('.stage');
+      // ...and do those sexy moves
+      // (dancer.step will be called on a timer)
+      setInterval(dancer.step, dancer.frequency);
+    },
+
+    step: function(){
+      dancer.getInPosition();
+      dancer.blink();
+    },
+
+    getInPosition: function(){
+      /* Use css top and left properties to position our <span> tag
+       * where it belongs on the page. See http://api.jquery.com/css/
+       */
+      var styleObj = {
+        top: dancer.top,
+        left: dancer.left
+      };
+      dancer.$moneyMaker.css(styleObj);
+    },
+
+    blink: function(){
+      /* toggle() is a jQuery method to show/hide the <span> tag.
+       * See http://api.jquery.com/category/effects/ for this and
+       * other effects you can use on a jQuery-wrapped html tag. */
+      dancer.$moneyMaker.toggle();
+    }
+
+  }; // end dancer
+
+  dancer.getInPosition();
+
+  return dancer;
+};
