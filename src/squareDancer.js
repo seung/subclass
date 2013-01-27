0c4eab4e3ddfcbb3328e360e9d63f6738c6c5519
Catalyst Students <accounts@catalystclass.com>
2013-01-26 16:25:19 -0800

    DANCING WORMS!! square with toggled animate

--- src/squareDancer.js
+++ src/squareDancer.js
@@ -3,6 +3,21 @@ var makeSquareDancer = function (top, left){
 
   squareDancer.$moneyMaker.addClass('square');
 
+
+  squareDancer.blink = function() {
+
+    // squareDancer.$moneyMaker.fadeToggle('slow');
+    // squareDancer.$moneyMaker.css('-webkit-transform: rotate(135deg)');
+
+    squareDancer.$moneyMaker.toggle(function(){
+      $(this).animate({
+      height: '25%'
+      // -webkit-transform: rotate(135deg),
+      // transform: rotate(135deg)
+    });
+    });
+  };
+
   return squareDancer;
 
 };
