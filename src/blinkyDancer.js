40e2724b870ff63134713b0c8562d8c670c6288e
Catalyst Students <accounts@catalystclass.com>
2013-01-27 12:21:18 -0800

    Blinky dancer prototype object works

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,4 +1,7 @@
-var dance = function() {
+var dancerPrototype = {};
+
+
+dancerPrototype.dance = function() {
   this.$moneyMaker.appendTo('.stage');
   var that = this;
 
@@ -12,12 +15,12 @@ var dance = function() {
   
 };
 
-var step = function() {
+dancerPrototype.step = function() {
   this.getInPosition();
   this.blink();
 };
 
-var getInPosition = function() {
+dancerPrototype.getInPosition = function() {
   var styleObj = {
     top: this.top,
     left: this.left
@@ -25,62 +28,16 @@ var getInPosition = function() {
   this.$moneyMaker.css(styleObj);
 };
 
-var blink = function() {
-  this.$moneyMaker.toggle();
+dancerPrototype.blink = function() {this.$moneyMaker.toggle();
 };
 
 var makeBlinkyDancer = function(left, top){
-  /* Creates and returns a new dancer object at the given position,
-   * where left is x-coordinate of left side and top is y-coordinate
-   * of top side (measured down from top of window). */
+  var dancer = Object.create(dancerPrototype);
+  dancer.left = left;
+  dancer.top = top;
+  dancer.frequency = Math.random() * 2000,
+  dancer.$moneyMaker = $('<span class="dancer"></span>');
 
-  var dancer = {
-    // we'll use top and left to set the position of this dancer
-    top: top,
-    left: left,
-
-    // used in setInterval below
-    frequency: Math.random() * 2000,
-
-    // get dressed... (use jQuery to create an HTML <span> tag)
-    $moneyMaker: $('<span class="dancer"></span>'),
-
-    // dance: function(){
-    //   // go out...  (add our tag to the HTML page)
-    //   dancer.$moneyMaker.appendTo('.stage');
-    //   // ...and do those sexy moves
-    //   // (dancer.step will be called on a timer)
-    //   setInterval(dancer.step, dancer.frequency);
-    // },
-    dance: dance,
-    step: step,
-    getInPosition: getInPosition,
-    blink: blink
-    // step: function(){
-    //   dancer.getInPosition();
-    //   dancer.blink();
-    // },
-
-    // getInPosition: function(){
-    //    Use css top and left properties to position our <span> tag
-    //    * where it belongs on the page. See http://api.jquery.com/css/
-       
-    //   var styleObj = {
-    //     top: dancer.top,
-    //     left: dancer.left
-    //   };
-    //   dancer.$moneyMaker.css(styleObj);
-    // },
-
-    // blink: function(){
-    //    toggle() is a jQuery method to show/hide the <span> tag.
-    //    * See http://api.jquery.com/category/effects/ for this and
-    //    * other effects you can use on a jQuery-wrapped html tag. 
-    //   dancer.$moneyMaker.toggle();
-    // }
-
-  }; // end dancer
   dancer.getInPosition();
-
   return dancer;
 };
