d69bc55989277f84322b9bf5e9b034c7f67f089c
Catalyst Students <accounts@catalystclass.com>
2013-01-27 12:06:26 -0800

    Add prototype chaining to dancer objects

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,3 +1,11 @@
+var lineUp = function(direction) {
+  for (var i = 0; i < window.dancers.length; i++) {
+    if (window.dancers[i].canLineUp) {
+      window.dancers[i].left = 0;
+    };
+  };
+};
+
 var dance = function (){
   this.$moneyMaker.appendTo('.stage');
   // console.log(this);
@@ -24,14 +32,6 @@ var blink = function(){
   this.$moneyMaker.toggle();
 };
 
-var lineUp = function(direction) {
-  for (var i = 0; i < window.dancers.length; i++) {
-    if (window.dancers[i].canLineUp) {
-      window.dancers[i].left = 0;
-    };
-  };
-};
-
 var walk = function(top, left){
   var floorWidth = $("body").width();
   var floorHeight = $("body").height();
@@ -47,69 +47,51 @@ var walk = function(top, left){
   return true;
 };
 
+var metaDancer = {};
+metaDancer.dance = dance;
+metaDancer.step = step;
+metaDancer.getInPosition = getInPosition;
+metaDancer.blink = blink;
+metaDancer.walk = walk;
+metaDancer.canLineUp = false;
+metaDancer.frequency = Math.random() * 2000;
+metaDancer.$moneyMaker = $('<span class="dancer"></span>');
 
 var makeBlinkyDancer = function(left, top){
   /* Creates and returns a new dancer object at the given position,
    * where left is x-coordinate of left side and top is y-coordinate
    * of top side (measured down from top of window). */
 
-  var dancer = {
-    // we'll use top and left to set the position of this dancer
-    top: top,
-    left: left,
-
-    canLineUp: false,
-    
-    // used in setInterval below
-    frequency: Math.random() * 2000,
-
-    // get dressed... (use jQuery to create an HTML <span> tag)
-    $moneyMaker: $('<span class="dancer"></span>'),
-
-    dance: dance,
-
-    step: step, 
-
-    getInPosition: getInPosition,
-
-    blink: blink
-
-  }; // end dancer
-
+  var dancer = Object.create(metaDancer);
+  dancer.top = top;
+  dancer.left = left;
   dancer.getInPosition();
 
   return dancer;
 };
 
 var makeMuffinDancer = function(left, top) {
+
   var muffinDancer = makeBlinkyDancer(left, top);
-
   muffinDancer.frequency = 500;
-
-  muffinDancer.canLineUp = false;
 
   // muffinDancer.$moneyMaker = $('<img src="img/MrCupcake.png" class="muffin-dancer">');
   muffinDancer.$moneyMaker = $('<span class="muffin-dancer"></span>');
-
   muffinDancer.getInPosition();
-
   return muffinDancer;
 }
 
 
 var makeRabiesDancer = function(left, top) {
+
   var rabiesDancer = makeBlinkyDancer(left, top);
-
   rabiesDancer.canLineUp = true;
-
   rabiesDancer.frequency = 35;
-
   // rabiesDancer.$moneyMaker = $('<img src="img/Toter.png" class="rabies-dancer">');
   rabiesDancer.$moneyMaker = $('<span class="rabies-dancer"></span>');
-
+  
   rabiesDancer.dX = 10;
   rabiesDancer.dY = 10;
-  rabiesDancer.walk = walk;
 
   rabiesDancer.step = function() {
     rabiesDancer.getInPosition();
