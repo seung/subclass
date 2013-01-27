a29c40e2a4197303f503c145a5f7b4d24e299afa
Catalyst Students <accounts@catalystclass.com>
2013-01-27 11:21:48 -0800

    about to take a break

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,3 +1,36 @@
+var dance = function(){
+  // go out...  (add our tag to the HTML page)
+  this.$moneyMaker.appendTo('.stage');
+  // ...and do those sexy moves
+  // (dancer.step will be called on a timer)
+  var that = this;
+  setInterval(function(){
+    that.step();
+  }, this.frequency);
+}
+
+var step = function(){
+      this.getInPosition();
+      this.blink();
+    }
+var blinkyPosition = function(){
+  /* Use css top and left properties to position our <span> tag
+   * where it belongs on the page. See http://api.jquery.com/css/
+   */
+var styleObj = {
+  top: this.top,
+  left: this.left
+  };
+  this.$moneyMaker.css(styleObj);
+};
+
+var blink = function(){
+  /* toggle() is a jQuery method to show/hide the <span> tag.
+   * See http://api.jquery.com/category/effects/ for this and
+   * other effects you can use on a jQuery-wrapped html tag. */
+  this.$moneyMaker.toggle();
+};
+
 var makeBlinkyDancer = function(left, top){
   /* Creates and returns a new dancer object at the given position,
    * where left is x-coordinate of left side and top is y-coordinate
@@ -14,36 +47,13 @@ var makeBlinkyDancer = function(left, top){
     // get dressed... (use jQuery to create an HTML <span> tag)
     $moneyMaker: $('<span class="dancer"></span>'),
 
-    dance: function(){
-      // go out...  (add our tag to the HTML page)
-      dancer.$moneyMaker.appendTo('.stage');
-      // ...and do those sexy moves
-      // (dancer.step will be called on a timer)
-      setInterval(dancer.step, dancer.frequency);
-    },
+    dance: dance,
 
-    step: function(){
-      dancer.getInPosition();
-      dancer.blink();
-    },
+    step: step,
 
-    getInPosition: function(){
-      /* Use css top and left properties to position our <span> tag
-       * where it belongs on the page. See http://api.jquery.com/css/
-       */
-      var styleObj = {
-        top: dancer.top,
-        left: dancer.left
-      };
-      dancer.$moneyMaker.css(styleObj);
-    },
+    getInPosition: blinkyPosition,
 
-    blink: function(){
-      /* toggle() is a jQuery method to show/hide the <span> tag.
-       * See http://api.jquery.com/category/effects/ for this and
-       * other effects you can use on a jQuery-wrapped html tag. */
-      dancer.$moneyMaker.toggle();
-    }
+    blink: blink
 
   }; // end dancer
 
