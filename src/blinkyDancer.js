1c223cbb7872666dfdaa29c61c78f7f0da10798d
Catalyst Students <accounts@catalystclass.com>
2013-01-27 13:03:51 -0800

    adds object.prototype

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,27 +1,28 @@
+//use object called blinkyDancer and whateverobect.create
+
+var dancer = {
+  
+  dance: globalDance,
+  step: globalStep,
+  getInPosition: globalGetInPosition,
+  blink: globalBlink,
+
+}; // end dancer
+
 var makeBlinkyDancer = function(left, top){
   /* Creates and returns a new dancer object at the given position,
    * where left is x-coordinate of left side and top is y-coordinate
    * of top side (measured down from top of window). */
+  
+  blinkyDancer = Object.create(dancer);
+  // we'll use top and left to set the position of this dancer
+  blinkyDancer.top = top;
+  blinkyDancer.left = left;
+  
+  blinkyDancer.frequency = Math.random() * 2000;
+  // get dressed... (use jQuery to create an HTML <span> tag)
+  blinkyDancer.$moneyMaker = $('<span class="dancer"></span>');
+  blinkyDancer.getInPosition();
 
-  var dancer = {
-    // we'll use top and left to set the position of this dancer
-    top: top,
-    left: left,
-
-    // used in setInterval below
-    frequency: Math.random() * 2000,
-
-    // get dressed... (use jQuery to create an HTML <span> tag)
-    $moneyMaker: $('<span class="dancer"></span>'),
-
-    dance: globalDance,
-    step: globalStep,
-    getInPosition: globalGetInPosition,
-    blink: globalBlink,
-
-  }; // end dancer
-
-  dancer.getInPosition();
-
-  return dancer;
+  return blinkyDancer;
 };
