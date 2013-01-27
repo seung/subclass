c8adee103fcf6fd6131915470a33c3ef608d4b2b
Catalyst Students <accounts@catalystclass.com>
2013-01-27 12:26:17 -0800

    Moved functions outsite of dancer so they are only called once.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,3 +1,30 @@
+var blink = function(){
+  this.$moneyMaker.toggle();
+};
+
+var dance = function(){
+  this.$moneyMaker.appendTo('.stage');
+  var that = this;
+  setInterval(function(){ that.step(); }, that.frequency);
+};
+
+var step = function(){
+  this.getInPosition();
+  this.blink();
+};
+
+var getInPosition = function(){
+  var styleObj = {
+    top: this.top,
+    left: this.left
+  };
+  this.$moneyMaker.css(styleObj);
+};
+
+var lineUp = function(){
+  this.left = 0;
+};
+
 var makeBlinkyDancer = function(left, top){
   /* Creates and returns a new dancer object at the given position,
    * where left is x-coordinate of left side and top is y-coordinate
@@ -7,49 +34,15 @@ var makeBlinkyDancer = function(left, top){
     // we'll use top and left to set the position of this dancer
     top: top,
     left: left,
-
     // used in setInterval below
     frequency: Math.random() * 2000,
-
     // get dressed... (use jQuery to create an HTML <span> tag)
     $moneyMaker: $('<span class="dancer"></span>'),
-
-    dance: function(){
-      // go out...  (add our tag to the HTML page)
-      dancer.$moneyMaker.appendTo('.stage');
-      // ...and do those sexy moves
-      // (dancer.step will be called on a timer)
-      setInterval(dancer.step, dancer.frequency);
-    },
-
-    step: function(){
-      dancer.getInPosition();
-      dancer.blink();
-    },
-
-    getInPosition: function(){
-      /* Use css top and left properties to position our <span> tag
-       * where it belongs on the page. See http://api.jquery.com/css/
-       */
-      var styleObj = {
-        top: dancer.top,
-        left: dancer.left
-      };
-      dancer.$moneyMaker.css(styleObj);
-    },
-
-    blink: function(){
-      /* toggle() is a jQuery method to show/hide the <span> tag.
-       * See http://api.jquery.com/category/effects/ for this and
-       * other effects you can use on a jQuery-wrapped html tag. */
-      dancer.$moneyMaker.toggle();
-    },
-
-    lineUp: function(){
-      this.left = 0;
-    }
-
-
+    step: step,
+    getInPosition: getInPosition,
+    blink: blink,
+    dance: dance,
+    lineUp: lineUp
   }; // end dancer
 
   dancer.getInPosition();
--- src/flameDancer.js
+++ src/flameDancer.js
@@ -2,13 +2,9 @@ var makeFlameDancer = function (top, left){
   var flameDancer = makeBlinkyDancer(top, left);
 
   flameDancer.$moneyMaker.addClass('burn');
-  $('body').burn();
+  $('div.flame').burn();
 
   flameDancer.blink = function() {
-
-    // flameDancer.$moneyMaker.fadeToggle('slow');
-    // flameDancer.$moneyMaker.css('-webkit-transform: rotate(135deg)');
-
     flameDancer.$moneyMaker.toggle(function(){
       $(this).text('BURN!').burn();
     });
