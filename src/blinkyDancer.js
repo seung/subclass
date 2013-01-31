01c784d61043a572a33ea32bdaaeb8a76ce7a5cf
Tony Thomson <thomson@me.com>
2013-01-30 21:51:54 -0800

    Pseudoclassical inheritance: Refactored to create prototypes and constructors for use with "new".

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,26 +1,24 @@
-var makeBlinkyDancer = function(left, top){
+var BlinkyDancer = function(left, top){
   /* Creates and returns a new blinkyDancer object at the given position,
    * where left is x-coordinate of left side and top is y-coordinate
    * of top side (measured down from top of window). */
-  
-  blinkyDancer = Object.create(blinkyDancerPrototype);
-  blinkyDancer.left = left;
-  blinkyDancer.top = top;
-  blinkyDancer.frequency = Math.random() * 2000;
-  blinkyDancer.$moneyMaker = $('<span class="dancer"></span>');
-  blinkyDancer.getInPosition();
-  return blinkyDancer;
+  this.left = left;
+  this.top = top;
+  this.frequency = Math.random() * 2000;
+  this.$moneyMaker = $('<span class="dancer"></span>');
+  this.getInPosition();
 };
 
-var blinkyDancerPrototype = Object.create(dancerPrototype);
-// get dressed... (use jQuery to create an HTML <span> tag)
-blinkyDancerPrototype.blink = function(){
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+
+BlinkyDancer.prototype.blink = function(){
   /* toggle() is a jQuery method to show/hide the <span> tag.
    * See http://api.jquery.com/category/effects/ for this and
    * other effects you can use on a jQuery-wrapped html tag. */
   this.$moneyMaker.toggle();
 };
-blinkyDancerPrototype.step = function(){
+
+BlinkyDancer.prototype.step = function(){
   this.getInPosition();
   this.blink();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -1,35 +1,32 @@
-var makeDancer = function(left, top) {
-  dancer = Object.create(dancerPrototype);
-  dancer.left = left;
-  dancer.top = top;
-  dancer.frequency = Math.random() * 2000;
-  dancer.$moneyMaker = $('<span class="dancer"></span>');
+var Dancer = function(left, top) {
+  // dancer = Object.create(dancerPrototype);
+  this.left = left;
+  this.top = top;
+  this.frequency = Math.random() * 2000;
+  this.$moneyMaker = $('<span class="this"></span>');
   blinkyDancer.getInPosition();
-  return dancer;
 }
 
-var dancerPrototype = {
-	dance: function(){
-    // go out...  (add our tag to the HTML page)
-    this.$moneyMaker.appendTo('.stage');
-    // ...and do those sexy moves
-    // (this.step will be called on a timer)
- 		var that = this;
-    setInterval(function() {that.step();}, this.frequency);
-  },
+Dancer.prototype.dance = function(){
+  // go out...  (add our tag to the HTML page)
+  this.$moneyMaker.appendTo('.stage');
+  // ...and do those sexy moves
+  // (this.step will be called on a timer)
+    var that = this;
+  setInterval(function() {that.step();}, this.frequency);
+};
 
-  step: function(){
-    this.getInPosition();
-  },
+Dancer.prototype.step = function(){
+  this.getInPosition();
+};
 
-  getInPosition: function(){
-    /* Use css top and left properties to position our <span> tag
-     * where it belongs on the page. See http://api.jquery.com/css/
-     */
-    var styleObj = {
-      top: this.top,
-      left: this.left
-    };
-    this.$moneyMaker.css(styleObj);
-  }
+Dancer.prototype.getInPosition = function(){
+  /* Use css top and left properties to position our <span> tag
+   * where it belongs on the page. See http://api.jquery.com/css/
+   */
+  var styleObj = {
+    top: this.top,
+    left: this.left
+  };
+  this.$moneyMaker.css(styleObj);
 };
