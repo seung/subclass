3529fd2b048f13d8005d4bb9b49b5a891dd371a0
Shawn Drost <sdrost@gmail.com>
2013-02-04 10:41:57 -0800

    use sprite prototype

--- src/Sprite.js
+++ src/Sprite.js
@@ -7,33 +7,36 @@ var Sprite = function(left, top){
     this.frequency = Math.random() * 2500;
     // get dressed...
     this.moneyMaker = $("<span class='blinkyDancer'></span>");
-    this.dance = function(){
-      // go out...
-      this.moneyMaker.appendTo(".stage");
-      // ...and do those sexy moves
-      setInterval(function(){this.step();}, this.frequency);
-    };
-    this.step = function(){
-      this.width = this.moneyMaker.width();
 
-      this.top = Math.random() * 900;
-      this.left = Math.random() * 900;
-      this.setStyles();
-
-      this.blink();
-    };
-    this.setStyles = function(){
-      var styleObj = {
-        width: this.width,
-        top: this.top,
-        left: this.left
-      };
-      this.moneyMaker.css(styleObj);
-    };
-    this.blink = function(){
-      // this = this;
-      this.moneyMaker.toggle();
-    }
-  
     this.setStyles();
+};
+
+Sprite.prototype = {
+  dance: function(){
+    // go out...
+    this.moneyMaker.appendTo(".stage");
+    // ...and do those sexy moves
+    setInterval(function(){this.step();}, this.frequency);
+  },
+  step: function(){
+    this.width = this.moneyMaker.width();
+
+    this.top = Math.random() * 900;
+    this.left = Math.random() * 900;
+    this.setStyles();
+
+    this.blink();
+  },
+  setStyles: function(){
+    var styleObj = {
+      width: this.width,
+      top: this.top,
+      left: this.left
+    };
+    this.moneyMaker.css(styleObj);
+  },
+  blink: function(){
+    // this = this;
+    this.moneyMaker.toggle();
+  }
 };
